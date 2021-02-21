5988e9d40ac76e830c163f7eaedf8fe9b9d76036
Paul Tim <paveltimofeenko@gmail.com>
2021-02-21 22:17:02 +0300

    harmonics time creation implemetation

--- index.py
+++ index.py
@@ -6,10 +6,14 @@ harmonics_count = 8
 frequency = 1500
 N = 1024
 
-signals = rsg.generateSignal(harmonics_count,frequency,N)
+signals,harmonics_time = rsg.generateSignal(harmonics_count,frequency,N)
 
 print("Expected value: ", np.mean(signals))
 print("Dispersion: ", np.std(signals))
+
+for i in range(harmonics_count):
+        print("harmonic {0} creation time: {1}s".format(i,harmonics_time[i]))
+
 
 fig, ax1 = plt.subplots()
 fig.set_size_inches(12,3)
@@ -20,3 +24,4 @@ ax1.set(xlabel='Time', ylabel='Signal(t)',
 
 fig.savefig("plot.png")
 plt.show()
+
--- rsg.py
+++ rsg.py
@@ -1,11 +1,15 @@
 import random
 import math
-
+from time import perf_counter
+ 
 
 def generateSignal(harmonics_count,frequency,N):
     signal = [0] * N
+    harmonics_time =  []
 
     for i in range (harmonics_count):
+        start_time = perf_counter()
+
         W = (frequency / harmonics_count) * (i+1)
         Amplitude = random.random()
         Phase = random.random()
@@ -13,4 +17,7 @@ def generateSignal(harmonics_count,frequency,N):
         for t in range(N):
             signal[t] += (Amplitude * math.sin(W * t + Phase))
 
-    return signal
+        harmonic_time = perf_counter() - start_time 
+        harmonics_time.append(harmonic_time)
+
+    return signal, harmonics_time
